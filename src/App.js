fca31db8fa71b07b91523229ec179ee43121051d
Younus Muradi <younusmuradi2023@gmail.com>
2025-10-28 12:19:39 +0430

    update

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import { useState } from "react";
+import Movies from "./component/Movies";
+function App() {
+  return (  
+    <main>
+      <Movies/>
+    </main>
 
-function Expense () {
-  const [expense ,setExpense] =useState([])
-  const [showExpense ,setShowExpense]= useState('')
-const Addexpense=()=>{
-  if(expense.trim() !== ""){
-    setShowExpense([...showExpense,expense]);
-    setExpense("")
-  }
+  );
 }
 
-
-  return ( 
-    <div>
-      <h1>Enter Your Expense</h1>
-      <input  
-      type="text"
-      value={expense}
-      placeholder="Write"
-      onChange={(e)=> setExpense(e.target.value)}
-      />
-      <button onClick={Addexpense}>Add Expense</button>
-      <ul >
-        {showExpense.map((n ,index) => (
-          <li key={index} >{n}</li>
-        ))}
-
-      </ul>
-    </div>
+export default App;
 
 
-   );
-}
 
 
-export default Expense;
